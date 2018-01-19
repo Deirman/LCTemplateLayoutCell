2e5c1573395c6e17d105238b8a15d94d72320b86
Deirman <727388890@qq.com>
2018-01-19 14:47:27 +0800

    Initial Commit

---
+++ LCTemplateLayoutCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCTemplateLayoutCell
+//
+//  Created by Deirman on 2018/1/19.
+//  Copyright © 2018年 Deirman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCTemplateLayoutCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCTemplateLayoutCell
+//
+//  Created by Deirman on 2018/1/19.
+//  Copyright © 2018年 Deirman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
